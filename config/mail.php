6a031637e3e79da51836a43e83c6a12cb07b0e0c
ldreyes8 <lreyes@habitatguate.org>
2017-08-07 12:59:28 -0600

    se agrego gmail

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'inforecursos@habitatguate.org'),
+        'address' => env('MAIL_FROM_ADDRESS', 'drdanielreyes5@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Notificacion RRHH Habitat'),
     ],
 
