bbcebec7636f2f7ad193c0713cdff97732e85f97
ldreyes8 <lreyes@habitatguate.org>
2017-10-11 15:25:15 -0600

    integro

--- public/assets/js/RH.js
+++ public/assets/js/RH.js
@@ -77,34 +77,8 @@ $(document).ready(function(){
             idempleado=$(this).val();
             var urlraiz=$("#url_raiz_proyecto").val();
 
-
-            $.get(urlraiz+'/rh/calculardias/'+idempleado,function(data){
-               
-                var horas = '';
-                var dias = '';
-                var tdh;
-
-                $.each(data,function(){
-                    horas = data[0];
-                    dias = data[1];
-                    autorizacion = data[2];
-                })
-
-                $('#inputTitle').html("Saldo de vacaciones");
-                $('#formAgregar').trigger("reset");
-                $('#formModal').modal('show');
-                $('#datomar').attr('disabled', 'disabled');
-                $('#hhoras').attr('disabled', 'disabled');
-                $('#dacumulado').attr('disabled', 'disabled');
-                $('#btnguardarV').attr('disabled', 'disabled'); 
-
-                tdh = (dias + ' ' + 'dias' + ' ' + 'con' +' '+ horas +' '+ 'horas');
-                document.getElementById('dacumulado').value = tdh;
-                document.getElementById('tdias').value = dias;
-                document.getElementById('thoras').value = horas;
-                
-            });
-        });
+            alert(urlraiz+'/rh/calculardias/'+idempleado);
+    });
 
     //Despido de un empleado
 
